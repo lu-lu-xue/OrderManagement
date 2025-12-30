d90c286b0305f51803d78ea8d83341fbd1f58f64
Lulu Xue <llxue@ucdavis.edu>
2025-12-29 22:33:44 -0800

    added circuit breaker in productServiceClient, and defined Logger using LoggerFactory to take care of log, and added fallbackInventoryCheck

--- src/main/java/com/beaconfire/ordermanagement/client/product/ProductServiceClient.java
+++ src/main/java/com/beaconfire/ordermanagement/client/product/ProductServiceClient.java
@@ -1,6 +1,9 @@
 package com.beaconfire.ordermanagement.client.product;
 
 import com.beaconfire.ordermanagement.dto.ProductDetailsDTO;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +17,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "product-service")
 @Validated
 public interface ProductServiceClient {
+	// define a static Logger
+	Logger log = LoggerFactory.getLogger(ProductServiceClient.class);
+	
 	// 1. check inventory
 	@GetMapping("/api/v1/products/availability")
+	@CircuitBreaker(
+			name = "productServiceCB",
+			fallbackMethod = "fallbackInventoryCheck"
+	)
 	boolean isProductAvailable(@RequestParam("productId") String productId, @RequestParam("quantity") Integer quantity);
+	
+	// fallback logic: what happens when the "Circuit is Open" or Service is down?
+	default boolean fallbackInventoryCheck(String id, Integer quantity, Throwable t){
+		log.error("Circuit Breaker triggered for Product Service. Reason: {}", t.getMessage());
+		
+		return false;
+	}
+	
 	
 	// 2. check price and name snapshotting
 	@GetMapping("/api/v1/products/{id}")
