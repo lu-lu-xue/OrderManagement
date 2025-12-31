ae6cdb185320854133cdc5f4976e024b858a919f
Lulu Xue <llxue@ucdavis.edu>
2025-12-31 11:41:45 -0800

    refactoring PaymentServiceClient and ProductServiceClient (more meaningful with fallbackMethod for fail-safe)

--- src/main/java/com/beaconfire/ordermanagement/client/payment/PaymentServiceClient.java
+++ src/main/java/com/beaconfire/ordermanagement/client/payment/PaymentServiceClient.java
@@ -1,15 +1,19 @@
 package com.beaconfire.ordermanagement.client.payment;
 
+import com.beaconfire.ordermanagement.client.product.ProductServiceClient;
 import com.beaconfire.ordermanagement.dto.PaymentRequestDTO;
 import com.beaconfire.ordermanagement.dto.PaymentResponseDTO;
+import com.beaconfire.ordermanagement.exception.PaymentServiceUnavailableException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.math.BigDecimal;
-import java.util.UUID;
 
 /**
  * @author luluxue
@@ -17,8 +21,22 @@ import java.util.UUID;
  */
 @FeignClient(name = "payment-service")
 public interface PaymentServiceClient {
+	// define a static Logger
+	Logger log = LoggerFactory.getLogger(PaymentServiceClient.class);
+	
+	// add fault tolerance strategy
 	@PostMapping("/api/v1/payment")
+	@Retry(name = "paymentRetry")
+	@CircuitBreaker(name = "paymentCB", fallbackMethod = "handlePaymentError")
 	PaymentResponseDTO chargeCustomer(PaymentRequestDTO requestDTto);
+	
+	default PaymentResponseDTO handlePaymentError(PaymentRequestDTO request, Throwable t){
+		log.error("Payment interface calling failed or CircuitBreaker is triggered. OrderId: {}, Reason: {}",
+				request.getOrderId(), t.getMessage());
+		
+		throw new PaymentServiceUnavailableException("Payment Service is unavailable, " +
+				"please check the order status later in the order page", t);
+	}
 	
 	@PostMapping("/api/v1/payments/{paymentId}")
 	ResponseEntity<PaymentResponseDTO> initiateRefund(
--- src/main/java/com/beaconfire/ordermanagement/client/product/ProductServiceClient.java
+++ src/main/java/com/beaconfire/ordermanagement/client/product/ProductServiceClient.java
@@ -1,6 +1,8 @@
 package com.beaconfire.ordermanagement.client.product;
 
 import com.beaconfire.ordermanagement.dto.ProductDetailsDTO;
+import feign.FeignException;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,8 +32,19 @@ public interface ProductServiceClient {
 	
 	// fallback logic: what happens when the "Circuit is Open" or Service is down?
 	default boolean fallbackInventoryCheck(String id, Integer quantity, Throwable t){
-		log.error("Circuit Breaker triggered for Product Service. Reason: {}", t.getMessage());
+		log.error("Failed to check stock for Product ID: {}," +
+				"Circuit Breaker triggered for Product Service. Reason: {}", id, t.getMessage());
 		
+		if (t instanceof CallNotPermittedException){
+			log.warn("Circuit is OPEN. Request for Product {} ignored to prevent system overload", id);
+		} else if (t instanceof FeignException.NotFound nfEx){
+			log.error("Product {} actually does not exist in ProductService. Status: {}", id, nfEx.status());
+		} else {
+			log.error("Generic failure for Product {}. Reason: {}", id, t.getMessage());
+		}
+		
+		// in e-commerce, returning false is "Fail-safe"
+		// because it prevents selling items we might not have
 		return false;
 	}
 	
